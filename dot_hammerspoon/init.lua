d5d4dc94e8ef1ae1cde1668178573a72c6e476fd
lepinkainen <riku.lindblad@iki.fi>
2024-05-06 11:56:50 +0300

    buch of hammerspoon setup for working at home

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -29,7 +29,12 @@ function arrangeTopTwoWindowsLeftRight()
         local max = screen:frame()
 
         -- Set window 1 to occupy the left half
-        win1:setFrame({x = max.x, y = max.y, w = max.w / 2, h = max.h}, 0)
+        win1:setFrame({
+            x = max.x,
+            y = max.y,
+            w = max.w / 2,
+            h = max.h
+        }, 0)
 
         -- Set window 2 to occupy the right half
         win2:setFrame({
@@ -41,6 +46,59 @@ function arrangeTopTwoWindowsLeftRight()
     end
 end
 
-hs.hotkey.bind(hyper, "3", function() arrangeTopTwoWindowsLeftRight() end)
+hs.hotkey.bind(hyper, "3", function()
+    arrangeTopTwoWindowsLeftRight()
+end)
 
-if hs.host.names()[1]:lower():find("mimic") then print("at home") end
+-- create a 2x2 grid with hs.grid
+hs.hotkey.bind(hyper, "4", function()
+    hs.grid.setGrid("2x2")
+    hs.grid.setMargins("0,0")
+    hs.grid.show()
+end)
+
+-- activate chooser
+hs.hotkey.bind(hyper, "5", function()
+    local choices = {}
+    for _, app in ipairs(hs.application.runningApplications()) do
+        table.insert(choices, {
+            text = app:name(),
+            subText = app:bundleID(),
+            bundleID = app:bundleID()
+        })
+    end
+    chooser:choices(choices)
+    chooser:show()
+end)
+
+-- list available displays
+-- hs.fnutils.each(hs.screen.allScreens(), function(screen) print(screen) end)
+
+if hs.host.names()[1]:lower():find("mimic") then
+    homeLayout()
+end
+
+-- To enable wifi network detection:
+-- type print(hs.location.get()) in the console
+-- go to System Preferences -> Security & Privacy -> Privacy -> Location Services
+-- check Hammerspoon
+if hs.host.names()[1]:lower():find("mystique") then
+    if hs.wifi.currentNetwork() == nil then
+        print("work mode - no wifi")
+    end
+    if hs.wifi.currentNetwork() == "Rocinante-5G" then
+        workAtHomeLayout()
+    end
+    if hs.wifi.currentNetwork() == "somethingelse" then
+        workAtOfficeLayout()
+    end
+
+    hs.notify.new({
+        autoWithdraw = true,
+        title = "Hammerspoon Layout",
+        informativeText = "Layout applied",
+        withdrawAfter = 2,
+        contentimage = hs.image.imageFromPath("/Users/riku.lindblad/Pictures/avatar.png"),
+        setIdImage = hs.image.imageFromPath("/Users/riku.lindblad/Pictures/avatar.png")
+    }):send()
+end
--- dot_hammerspoon/layouts.lua
+++ dot_hammerspoon/layouts.lua
@@ -1,25 +1,44 @@
 local hyper = {"cmd", "alt", "ctrl", "shift"}
 
--- Layouts
+local laptopScreen = "Built-in Retina Display"
+local mainDisplay = "L32p-30"
 local verticalScreen = "LEN P27h-10"
-local homeLayout = {
-    {
-        "Telegram", nil, verticalScreen, hs.geometry.unitrect(0, 0, 1, 0.5),
-        nil, nil
-    }, {
-        "Discord", nil, verticalScreen, hs.geometry.unitrect(0, 0.5, 1, 0.5),
-        nil, nil
-    }
-}
+local workDisplay = "FOO"
 
--- open application for home and arrange correctly
-hs.hotkey.bind(hyper, "1", function()
-    hs.application.launchOrFocus("Telegram")
-    hs.application.launchOrFocus("Discord")
+function homeLayout()
+    local homeLayout = {{"Telegram", nil, verticalScreen, hs.geometry.unitrect(0, 0, 1, 0.5), nil, nil},
+                        {"Discord", nil, verticalScreen, hs.geometry.unitrect(0, 0.5, 1, 0.5), nil, nil}}
+
+    hs.application.launchOrFocusByBundleID("ru.keepcoder.Telegram")
+    hs.application.launchOrFocusByBundleID("com.hnc.Discord")
+    hs.application.launchOrFocusByBundleID("md.obsidian")
+    hs.application.launchOrFocusByBundleID("com.apple.Music")
+
     hs.layout.apply(homeLayout)
-    hs.notify.new({
-        title = "Hammerspoon",
-        informativeText = "Home layout applied"
-    }):send()
+end
 
+function workAtHomeLayout()
+    local workAtHomeLayout =
+        {{"Telegram", nil, verticalScreen, hs.geometry.unitrect(0, 0, 1, 0.5), nil, nil}, -- top half
+        {"Discord", nil, verticalScreen, hs.geometry.unitrect(0, 0.5, 1, 0.5), nil, nil}, -- bottom half
+        {"Slack", nil, mainDisplay, hs.geometry.unitrect(0.5, 0, 0.5, 1), nil, nil}, -- right half
+        {"md.obsidian", nil, mainDisplay, hs.geometry.unitrect(0, 0, 0.5, 1), nil, nil}, -- left half        
+        {"iTerm2", nil, laptopScreen, hs.geometry.unitrect(0, 0, 1, 1), nil, nil}, -- full screen
+        {"Music", nil, laptopScreen, hs.geometry.unitrect(0, 0, 1, 1), nil, nil} -- full screen
+        }
+
+    -- find bundle id for Slack
+    -- hs.fnutils.each(hs.application.runningApplications(), function(app) print(app:bundleID()) end)
+    hs.application.launchOrFocusByBundleID("ru.keepcoder.Telegram")
+    hs.application.launchOrFocusByBundleID("com.hnc.Discord")
+    hs.application.launchOrFocusByBundleID("md.obsidian")
+    hs.application.launchOrFocusByBundleID("com.tinyspeck.slackmacgap")
+    hs.application.launchOrFocusByBundleID("com.apple.Music")
+    hs.application.launchOrFocusByBundleID("com.googlecode.iterm2")
+
+    hs.layout.apply(workAtHomeLayout)
+end
+
+hs.hotkey.bind(hyper, "1", function()
+    homeLayout()
 end)
