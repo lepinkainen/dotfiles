0143d00e00c31e1692bc5301fe78394078b3e723
lepinkainen <riku.lindblad@iki.fi>
2024-05-08 13:11:48 +0300

    Change daily note hotkey

--- dot_hammerspoon/applications.lua
+++ dot_hammerspoon/applications.lua
@@ -3,5 +3,5 @@ local hyper = {"cmd", "alt", "ctrl", "shift"}
 -- launch obsidian and open daily note with hotkey
 hs.hotkey.bind(hyper, "o", function()
     local app = hs.application.launchOrFocus("Obsidian")
-    hs.eventtap.keyStroke({"shift", "cmd"}, "d", 200000, app)
+    hs.eventtap.keyStroke(hyper, "d", 200000, app)
 end)
