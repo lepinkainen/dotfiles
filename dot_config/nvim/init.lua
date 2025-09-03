bacb03ca713f4d5c3b13504b402a1526648ba03f
lepinkainen <riku.lindblad@iki.fi>
2025-09-03 10:15:51 +0300

    More nvim setup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,3 +5,10 @@ vim.opt.relativenumber = true
 -- Show absolute line on active
 vim.opt.number = true
 
+-- These create custom shortcuts using leader
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle file tree" })
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Search in files" })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+
