ff957dd0dc6b72b831a7125603982d665862c863
lepinkainen <riku.lindblad@iki.fi>
2025-09-03 10:51:59 +0300

    Even more nvim crap

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -12,3 +12,12 @@ vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Search i
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
 
+-- Add these to your existing init.lua after the current keymaps
+vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format code" })
+vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic" })
+
+-- Mason UI shortcut
+vim.keymap.set("n", "<leader>m", ":Mason<CR>", { desc = "Mason LSP installer" })
+
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -4,12 +4,41 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require('telescope').setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", ".git", "vendor" },
+        }
+      })
+    end,
   },
-  -- LSP
-  { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim" },
   
-  -- Autocompletion
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
+  -- Commenting with <leader>cc
+  { "preservim/nerdcommenter" },
+  
+  -- Git integration
+  { "tpope/vim-fugitive" },
+  
+  -- Git decorations in gutter  
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end,
+  },
+
+  -- Status line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require('lualine').setup({
+        options = {
+          theme = 'auto',
+          section_separators = '',
+          component_separators = '',
+        }
+      })
+    end,
+  },
 }
